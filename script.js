a5dd9ec997052d08b9bb843f3583c284d6527254
harissaif <haris.saif@gmail.com>
2020-06-25 19:40:26 +0800

    done!

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function computerPlay(){
+    document.getElementById("text").innerHTML = "Result!";
     var x = Math.floor((Math.random() * 3) + 1);
     var computerSelection;
     if(x == 1){
@@ -12,10 +13,18 @@ function computerPlay(){
     var y = prompt("Enter your choice");
     
     
-    if(computerSelection == y.toString()){
-        document.getElementById("text").innerHTML = "You Won!";
-    }else{
-        document.getElementById("text").innerHTML = "You Loose!";
+    if(computerSelection ==  "rock" && y.toString() == "paper"){
+        document.getElementById("text").innerHTML = "You Won! Paper beats Rock!";
+    }else if(computerSelection ==  "paper" && y.toString() == "rock"){
+        document.getElementById("text").innerHTML = "You Loose! Paper beats rock!";
+    }else if(computerSelection ==  "scissor" && y.toString() == "paper"){
+        document.getElementById("text").innerHTML = "You Loose! scissor beats paper!";
+    }else if(computerSelection ==  "paper" && y.toString() == "scissor"){
+        document.getElementById("text").innerHTML = "You Won! scissor beats paper!";
+    }else if(computerSelection ==  "rock" && y.toString() == "scissor"){
+        document.getElementById("text").innerHTML = "You Loose! rock beats Scissor!";
+    }else if(computerSelection ==  "scissor" && y.toString() == "rock"){
+        document.getElementById("text").innerHTML = "You Won! Rock beats Scissor!";
     }
 
 }
